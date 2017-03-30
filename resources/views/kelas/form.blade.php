d31c158ecb0e33a69cd9a23db50ff2d9c0d55b13
alvaigan <aalvaigan@gmail.com>
2017-03-30 20:02:25 +0700

    versi awal

---
+++ resources/views/kelas/form.blade.php
@@ -0,0 +1,53 @@
+@extends('template.header')
+
+@section('content')
+<!-- Content Header (Page header) -->
+<section class="content-header">
+  <h1>
+	Tambah Data Kelas
+	<small>SMK Negeri 4 Bandung</small>
+  </h1>
+  <ol class="breadcrumb">
+	<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+	<li><a href="#">Data</a></li>
+	<li class="active">Tambah Data Kelas</li>
+  </ol>
+</section>
+
+<!-- Main content -->
+<section class="content">
+
+  <!-- Default box -->
+  <div class="box">
+	<div class="box-header with-border">
+	  <a href="" class="btn btn-info"><i class="fa fa-chevron-left"></i> Kembali</a>
+
+	  <div class="box-tools pull-right">
+		<button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
+		  <i class="fa fa-minus"></i></button>
+		<button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
+		  <i class="fa fa-times"></i></button>
+	  </div>
+	</div>
+	<div class="box-body">
+	  
+	</div>
+	<!-- /.box-body -->
+	<div class="box-footer">
+	  
+	</div>
+	<!-- /.box-footer-->
+  </div>
+  <!-- /.box -->
+
+</section>
+<!-- /.content -->
+@endsection
+
+@push('style')
+<!-- <link rel="stylesheet" href="datepicker.css" /> -->
+@endpush
+
+@push('script')
+<!-- <script type="text/javascritp" href="datepicker.js"></script> -->
+@endpush
